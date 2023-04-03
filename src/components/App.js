096959f11eeb8c7f40737046aff8e5dc04e5cff3
RemyXVX <remyfranciscoflores@gmail.com>
2023-04-03 14:11:02 -0700

    made some style changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import CoffeeControl from "./CoffeeControl";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
-      <CoffeeControl />
+      <div class="container">
+        <div class="first">
+          <div class="second">
+            <Header />
+            <CoffeeControl />
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import React from "react";
 const Header = () => {
     return (
         <React.Fragment>
-            <h1>Welcome to Doom Coffe Shop</h1>
-            <p>home of the overly caffienated and lost dreams.</p>
+            <div class="header">
+                <h1>Welcome to Doom Coffee Shop</h1>
+                <p>home of the overly caffienated and lost dreams.</p>
+            </div>
         </React.Fragment>
     )
 }
